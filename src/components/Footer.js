c8f14ea9c90a3d79c5ec15b9f11ba47121143cf3
BranLWill <bwillett@knights.ucf.edu>
2023-09-29 17:21:57 -0400

    Initial Auth0 setup

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,15 @@
 import React from "react";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 const Footer = () => (
   <footer className="bg-light p-3 text-center">
     <div className="logo" />
+    {/* <div>
+      <FontAwesomeIcon icon={faEnvelope} />
+    </div> */}
     <p>
-      Sample project provided by <a href="https://auth0.com">Auth0</a>
+      User authentication provided by <a href="https://auth0.com">Auth0</a>
     </p>
   </footer>
 );
